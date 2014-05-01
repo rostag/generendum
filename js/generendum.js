e955be9938c30014c62337482d37d4ea1846bee7
Rost Siryk <rostislav.siryk@gmail.com>
2014-05-01 04:11:26 +0300

    generendum version zero

---
+++ js/generendum.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+	"use strict";
+	function getOptions() {
+		var action;
+		var actions = [
+			[ "Сохранение", "в составе" ],
+			[ "Включение", "в состав" ],
+			[ "Исключение", "из состава" ],
+			[ "Отключение", "от" ],
+			[ "Федерализация", "независимо от" ]
+		];
+		var names = [ "Украины", "т.н. ДНР", "РФ", "Днепропетровской области", "Януковича", "Крыма", "трубы" ];
+		var ofThe = [ "Украины", "т.н. ДНР", "РФ", "Днепропетровской области", "ЕС", "трубы", "интернета", "Русского Мира", "Крыма", "обстоятельств" ];
+		var creations = [ "Провозглашение", "Похищение", "Оповещение", "Облучение", "Объявление", "Признание" ];
+		var beingThe = [ "независимым государством", "наследником СССР", "агентом ЦРУ", "пособником терроризма", "невиновным" ];
+		// Math.random().toString(36).substr(2);	
+		function getName() {
+			var name = names.splice( idx = Math.floor( Math.random() * names.length ), 1 );
+			var idx = ofThe.indexOf( name );
+			ofThe.splice( idx, 1 );
+			return name;
+		}
+		function getAction() {
+			action = actions.splice( Math.floor( Math.random() * actions.length ), 1 )[0];
+			return action[0] + " " + name;
+		}
+		function getInThe() {
+			return action[1];
+		}
+		function getOfThe() {
+			return ofThe.splice( Math.floor( Math.random() * ofThe.length ), 1 );
+		}
+		function getBeingThe() {
+			return beingThe.splice( Math.floor( Math.random() * beingThe.length ), 1 );
+		}
+		function getCreation() {
+			return creations.splice( Math.floor( Math.random() * creations.length ), 1 ) + " " + name;
+		}
+
+		var name = getName();
+
+		$(".opt1").html( "1. " + getAction() + " " + getInThe() + " " + getOfThe() );
+		$(".opt2").html( "2. " + getAction() + " " + getInThe() + " " + getOfThe() );
+		$(".opt3").html( "3. " + getCreation() + " " + getBeingThe() );
+	}
+
+	var $generate = $(".btn-generate");
+	var $result = $(".options-wrapper");
+
+	$generate.bind( "click", function(){
+		getOptions();
+		$result.show();
+	});
+});
